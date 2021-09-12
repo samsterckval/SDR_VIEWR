a29031d90d64aba6699876b0de8a4e5080b21d56
Sam Sterckval <sam@edgise.com>
2021-09-12 12:35:06 +0200

    all settings now update + new layout

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QTextEdit, QLineEdit
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton
 from PyQt5.QtGui import QPixmap
 import cv2
 from PyQt5.QtCore import pyqtSlot, Qt
 import numpy as np
-from framegrabber_thread import VideoThread
+from framegrabber_thread import VideoThread, SdrSettings
 import matplotlib.pyplot as plt
 from utils import get_img_array
 
@@ -12,34 +12,86 @@ from utils import get_img_array
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Qt live label demo")
-        self.disply_width = 640
-        self.display_height = 480
+        self.setWindowTitle("~ Edgise Live SDR ~")
+        self.setMinimumWidth(900)
+        self.display_width = 500
+        self.display_height = 500
+
+        self.sdr_settings = SdrSettings()
+
         # create the label that holds the image
         self.image_label = QLabel(self)
-        self.image_label.resize(self.disply_width, self.display_height)
-        # create a text label
-        self.textLabel = QLabel('FFT')
+        self.image_label.resize(self.display_width, self.display_height)
+
+        self.update_button = QPushButton('Update')
+
+        fixed_width_labels = 80
+        self.nfft_label = QLabel('NFFT')
+        self.nfft_label.setFixedWidth(fixed_width_labels)
+        self.gain_label = QLabel('gain')
+        self.gain_label.setFixedWidth(fixed_width_labels)
+        self.bandwidth_label = QLabel('bandwidth')
+        self.bandwidth_label.setFixedWidth(fixed_width_labels)
+        self.read_label = QLabel('read size')
+        self.read_label.setFixedWidth(fixed_width_labels)
+        self.samplefreq_label = QLabel('sample freq')
+        self.samplefreq_label.setFixedWidth(fixed_width_labels)
+        self.centerfreq_label = QLabel('center freq')
+        self.centerfreq_label.setFixedWidth(fixed_width_labels)
 
         self.nfft_input = QLineEdit("1024")
-        self.fs_input = QLineEdit("30000")
-        self.fc_input = QLineEdit("470625000")
+        self.gain_input = QLineEdit("4")
+        self.bandwidth_input = QLineEdit("30000")
+        self.read_input = QLineEdit("262144")
+        self.samplefreq_input = QLineEdit("2400000")
+        self.centerfreq_input = QLineEdit("470625000")
+
+        nfft_layout = QHBoxLayout()
+        nfft_layout.addWidget(self.nfft_label)
+        nfft_layout.addWidget(self.nfft_input)
+
+        gain_layout = QHBoxLayout()
+        gain_layout.addWidget(self.gain_label)
+        gain_layout.addWidget(self.gain_input)
+
+        bandwidth_layout = QHBoxLayout()
+        bandwidth_layout.addWidget(self.bandwidth_label)
+        bandwidth_layout.addWidget(self.bandwidth_input)
+
+        read_layout = QHBoxLayout()
+        read_layout.addWidget(self.read_label)
+        read_layout.addWidget(self.read_input)
+
+        sample_layout = QHBoxLayout()
+        sample_layout.addWidget(self.samplefreq_label)
+        sample_layout.addWidget(self.samplefreq_input)
+
+        center_layout = QHBoxLayout()
+        center_layout.addWidget(self.centerfreq_label)
+        center_layout.addWidget(self.centerfreq_input)
+
+        vbox_right = QVBoxLayout()
+        vbox_right.addLayout(nfft_layout)
+        vbox_right.addLayout(gain_layout)
+        vbox_right.addLayout(bandwidth_layout)
+        vbox_right.addLayout(sample_layout)
+        vbox_right.addLayout(center_layout)
+        vbox_right.addLayout(read_layout)
+        vbox_right.addWidget(self.update_button)
 
         # create a vertical box layout and add the two labels
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.image_label)
-        vbox.addWidget(self.textLabel)
-        vbox.addWidget(self.nfft_input)
-        vbox.addWidget(self.fs_input)
-        vbox.addWidget(self.fc_input)
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.image_label)
+        hbox.addLayout(vbox_right)
 
         # set the vbox layout as the widgets layout
-        self.setLayout(vbox)
+        self.setLayout(hbox)
 
         # create the video capture thread
-        self.thread = VideoThread()
+        self.thread = VideoThread(self.sdr_settings)
         # connect its signal to the update_image slot
         self.thread.change_pixmap_signal.connect(self.update_image)
+        self.update_button.clicked.connect(self.update_settings)
         # start the thread
         self.thread.start()
 
@@ -47,15 +99,29 @@ class App(QWidget):
         self.thread.stop()
         event.accept()
 
+    def update_settings(self):
+        if self.gain_input.text() != "auto":
+            self.sdr_settings.gain = int(self.gain_input.text())
+        else:
+            self.sdr_settings.gain = 'auto'
+
+        self.sdr_settings.bandwidth = int(self.bandwidth_input.text())
+        self.sdr_settings.sample_rate = int(self.samplefreq_input.text())
+        self.sdr_settings.center_freq = int(self.centerfreq_input.text())
+        self.sdr_settings.nfft = int(self.nfft_input.text())
+        self.sdr_settings.DEFAULT_READ_SIZE = int(self.read_input.text())
+
+        self.thread.restart(self.sdr_settings)
+
     @pyqtSlot(np.ndarray)
     def update_image(self, samples):
         """Updates the image_label with a new opencv image"""
 
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10), dpi=100)  # two axes on figure
         new_psd, new_freq = plt.psd(samples,
-                                    NFFT=int(self.nfft_input.text()),
-                                    Fs=int(self.fs_input.text()),
-                                    Fc=int(self.fc_input.text()),
+                                    NFFT=self.sdr_settings.nfft,
+                                    Fs=self.sdr_settings.sample_rate,
+                                    Fc=self.sdr_settings.center_freq,
                                     color="black")
 
         ax.axis("off")
@@ -64,6 +130,7 @@ class App(QWidget):
 
         qt_img = self.convert_cv_qt(img)
         self.image_label.setPixmap(qt_img)
+        plt.close(fig)
 
     def convert_cv_qt(self, cv_img):
         """Convert from an opencv image to QPixmap"""
@@ -71,5 +138,5 @@ class App(QWidget):
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
+        p = convert_to_Qt_format.scaled(self.display_width, self.display_height, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
--- framegrabber_thread.py
+++ framegrabber_thread.py
@@ -1,39 +1,95 @@
 from PyQt5.QtCore import QThread, pyqtSignal
 import numpy as np
-from rtlsdr import *
+try:
+    from rtlsdr import RtlSdr
+except Exception as e:
+    import time
+    print(f"rtlsdr not found : {e}")
+
+
+class SdrSettings:
+
+    def __init__(self,
+                 sample_rate: int = 24e5,
+                 center_freq: int = 470625e3,
+                 gain: int = 4,
+                 bandwidth: int = 3000,
+                 DEFAULT_READ_SIZE: int = 256*1024,
+                 nfft: int = 1024):
+
+        self.sample_rate = sample_rate
+        self.center_freq = center_freq
+        self.gain = gain
+        self.bandwidth = bandwidth
+        self.DEFAULT_READ_SIZE = DEFAULT_READ_SIZE
+        self.nfft = nfft
+
+    def __str__(self):
+        return f"SDR Settings :\n" \
+               f"--------------------\n" \
+               f"sample rate : {self.sample_rate}\n" \
+               f"center freq : {self.center_freq}\n" \
+               f"gain        : {self.gain}\n" \
+               f"bandwidth   : {self.bandwidth}\n" \
+               f"read size   : {self.DEFAULT_READ_SIZE}\n" \
+               f"nfft        : {self.nfft}\n"
 
 
 class VideoThread(QThread):
     change_pixmap_signal = pyqtSignal(np.ndarray)
+    change_settings_signal = pyqtSignal(SdrSettings)
 
-    def __init__(self):
+    def __init__(self, sdr_settings: SdrSettings):
         super().__init__()
         self._run_flag = True
 
-        self.sample_rate = 24e5
-        self.center_freq = 470625e3
-        self.gain = 4
-        self.bandwidth = 3000
+        self.sdr_settings: SdrSettings = sdr_settings
 
     def run(self):
-        sdr = RtlSdr()
+        print(f"Running with settings : \n{self.sdr_settings}")
+        ok = False
+        try:
+            sdr = RtlSdr()
+            ok = True
+        except Exception as e:
+            print(f"Exception during radio opening : {e}")
+            pass
         # configure device
-        sdr.sample_rate = self.sample_rate
-        sdr.center_freq = self.center_freq
-        sdr.gain = self.gain
-        sdr.bandwidth = self.bandwidth
+        # sdr.sample_rate = self.sdr_settings.sample_rate
+        # sdr.center_freq = self.sdr_settings.center_freq
+        # sdr.gain = self.sdr_settings.gain
+        # sdr.bandwidth = self.sdr_settings.bandwidth
+        #
+        # Below should be a shorter, cleaner and cooler version of the above
+        if ok:
+            for key, val in self.sdr_settings.__dict__.items():
+                if key in sdr.__dict__.items():
+                    setattr(sdr, key, val)
 
-        while self._run_flag:
-            samples = sdr.read_samples(1024*256)
-            self.change_pixmap_signal.emit(samples)
+            while self._run_flag:
+                samples = sdr.read_samples()
+                self.change_pixmap_signal.emit(samples)
 
-        sdr.close()
+            sdr.close()
+            del sdr
+        else:
+            while self._run_flag:
+                x = np.linspace(-np.pi*10, np.pi*10, self.sdr_settings.DEFAULT_READ_SIZE)
+                samples = np.sin(x*np.random.randint(0, 100, 1))
+                self.change_pixmap_signal.emit(samples)
+                time.sleep(0.3)
+
+        print("Thread stopping")
 
     def stop(self):
         """Sets run flag to False and waits for thread to finish"""
         self._run_flag = False
         self.wait()
 
-    def restart(self):
+    def restart(self, new_settings: SdrSettings = None):
+        if new_settings is not None:
+            self.sdr_settings = new_settings
         self._run_flag = False
         self.wait()
+        self._run_flag = True
+        self.start()
